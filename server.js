71d249738ff601b81c9b2239ee52470462ee7033
Kei Namiri <knamiri@yahoo.com>
2018-04-07 14:59:42 -0700

    fixing Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express=require("express");
 var mongoose=require("mongoose");
 var cheerio=require("cheerio");
 var connection=require("./config/connection.js");
-
+var env = require('dotenv').load();
 const PORT=process.env.PORT | 5000;
 
 var app=express();
